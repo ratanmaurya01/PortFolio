05cf99d142690b655bb6e39c345cbe100d0889bf
Ratan Maurya <151978027+ratanmaurya01@users.noreply.github.com>
2024-11-18 10:09:11 +0530

    Update the header component

---
+++ portfolio/src/components/Project.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Project() {
+  return (
+    <div>
+        <div>
+            <div>
+                  <h6>this is this first project of then link and overViews</h6>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Project
